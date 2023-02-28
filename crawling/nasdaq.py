edee2f866c4f060f2ebdde6e81000c6f105a7aa5
william <william@akaai.kr>
2023-02-28 20:39:15 +0900

    work on another pc

--- crawling/nasdaq.py
+++ crawling/nasdaq.py
@@ -2,34 +2,46 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-import time
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import csv
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('window-size=1920,1080')
-
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.implicitly_wait(5)
 
 driver.get(url='https://www.nasdaq.com/')
-time.sleep(5)
+sleep(5)
 # search_box = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
 
 search_box = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div[2]/article/div/div[2]/div[2]/aside/nsdq-right-rail-desktop/div/div[1]/div/div[1]/form/div/div[2]/input')
-time.sleep(1)
-search_box.send_keys('clinical success')
+search_box.send_keys('check')
 search_box.send_keys(Keys.RETURN)
 
 driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div[2]/div[3]/div/section/div[2]/div[3]/div[3]/div[1]/span[2]/select').click()
 driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div[2]/div[3]/div/section/div[2]/div[3]/div[3]/div[1]/span[2]/select/option[2]').click()
 title_text = driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div[2]/div[3]/div/section/div[2]/div[3]/div[3]/div[2]/a[1]/div/div[1]').text
-print(title_text)
+print("title_text:", title_text)
 
 
+duplicate_check = []
 
-while(True):
-    pass
+with open('check_list.csv', 'r') as to_read:
+  reader = csv.reader(to_read)
+  for row in reader:
+    duplicate_check.append(row[0])
+
+print("duplicate_check:", duplicate_check)
+
+if title_text not in duplicate_check:
+  with open('check_list.csv', 'a', newline='') as to_write:
+    writer = csv.writer(to_write)
+    writer.writerow([title_text])
+
+while True:
+  pass
 
 
 
